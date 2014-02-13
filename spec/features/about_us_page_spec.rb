bebffcda903dc88457acc3ce5ee8aca1ef00fbf9
Lixang Ding <zhimadlx@gmail.com>
2014-02-13 15:46:23 +0800

    cc

--- spec/features/about_us_page_spec.rb
+++ spec/features/about_us_page_spec.rb
@@ -5,7 +5,7 @@ describe "About Page", sauce: true do
   #let!(:about_page) { create(:web_page, name: "about",path: "about", long_title: "about page", content: "about...")}
 
   it "have a right title" do
-    visit fap_url + '/about'
+    visit 'http://dev.findaport.com/about'
     expect(page).to have_content('Having launched in February 2010')
   end 
 end
