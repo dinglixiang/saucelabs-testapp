71c809c211832de5f87d5d6213f3164bbe705a2b
Lixang Ding <zhimadlx@gmail.com>
2014-02-13 15:51:06 +0800

    ad

--- spec/features/about_us_page_spec.rb
+++ spec/features/about_us_page_spec.rb
@@ -1,8 +1,6 @@
 require 'spec_helper'
 
 describe "About Page", sauce: true do
-  let(:fap_url) { "http://dev.findaport.com" }
-  #let!(:about_page) { create(:web_page, name: "about",path: "about", long_title: "about page", content: "about...")}
 
   it "have a right title" do
     visit 'http://dev.findaport.com/about'
