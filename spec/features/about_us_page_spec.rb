d19aab55a8d986c74575336c164d56e07bc7ec11
Lixang Ding <zhimadlx@gmail.com>
2014-02-14 11:12:37 +0800

    put

--- spec/features/about_us_page_spec.rb
+++ spec/features/about_us_page_spec.rb
@@ -6,7 +6,7 @@ describe "About Page", sauce: true do
   #end
 
   it "have a right title" do
-    p SauceOnDemandSessionID=selenium.session_id job-name=selenium.job_name
+    p "SauceOnDemandSessionID=" + selenium.session_id.to_s + "  " + "job-name=" + selenium.job_name.to_s
     visit 'http://dev.findaport.com/about'
     expect(page).to have_content('Having launched in February 2010')
   end 
